f77032d949734141fd1cce9b88fe35e3a087fe7a
Administrador <cespino1905@gmail.com>
2022-10-09 22:04:30 -0500

    Fisrt commit

---
+++ src/main/java/pe/ineapp/ineapppersonaservice/Persona/Persona.java
@@ -0,0 +1,49 @@
+package pe.ineapp.ineapppersonaservice.Persona;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+//1@AllArgsConstructor //Es como si tuvieramos creado el constructor
+//2@NoArgsConstructor //Permite tener el constructor sin argumentos
+//3@Setter //ya no es necesario crear los setter
+//4@Getter //ya no es necesario crear los getter
+//@Data // se puede simplificar (1-2-3-4)
+
+@Entity
+@Table(
+       name ="tbl_Persona",
+       uniqueConstraints = {
+               @UniqueConstraint(name="persona_email_unique",columnNames = "email"),
+               @UniqueConstraint(name="persona_dni_unique",columnNames = "dni"),
+       }
+)
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Persona {
+    @SequenceGenerator(
+            name="persona_sequence",
+            sequenceName = "persona_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "persona_sequence"
+    )
+    @Id
+    Long id;
+    String name;
+    String lastname;
+    String dni;
+    String email;
+    LocalDate birthDate;
+
+}
---
+++ src/main/java/pe/ineapp/ineapppersonaservice/Persona/PersonaController.java
@@ -0,0 +1,38 @@
+package pe.ineapp.ineapppersonaservice.Persona;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping("/persona")
+public class PersonaController {
+
+    @GetMapping
+    @RequestMapping("/getAll")
+    public List<Persona> getPersona(){
+
+        LocalDate date = LocalDate.of(2020, Month.APRIL,4);
+        Persona persona = Persona.builder()
+                .id(1L)
+                .name("Christian")
+                .lastname("Espinoza")
+                .dni("45966557")
+                .email("cespinoza@central.com.pe")
+                .birthDate(date).build();
+        Persona persona1 = Persona.builder()
+                .id(2L)
+                .name("Mathias")
+                .lastname("Espinoza")
+                .dni("89349483")
+                .email("mespinoza@central.com.pe")
+                .birthDate(date).build();
+
+        return List.of(persona,persona1);
+    }
+}
